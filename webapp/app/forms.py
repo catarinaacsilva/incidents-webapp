ceeb64de903a272074e6f6bfe97f4b7c1e433041
catarina <c.alexandracorreia@ua.pt>
2019-10-24 17:42:39 +0100

    First commit

---
+++ webapp/app/forms.py
@@ -0,0 +1,65 @@
+from django import forms
+from bootstrap_datepicker_plus import DatePickerInput
+
+STATE = [('a','Em Resolução'),
+         ('b','Conclusão'),
+         ('c','Encerrada'),
+         ('d','Em curso'),
+         ('e','Despacho de 1º Alerta')
+         ]
+DISTRITOS = [('a','Aveiro'),
+             ('b', 'Beja'),
+             ('c', 'Braga'),
+             ('d','Bragança'),
+             ('e','Castelo Branco'),
+             ('f','Coimbra'),
+             ('g','Évora'),
+             ('h','Faro'),
+             ('i','Guarda'),
+             ('j','Leiria'),
+             ('k','Lisboa'),
+             ('l','Portalegre'),
+             ('m','Porto'),
+             ('n','Santarém'),
+             ('o','Setúbal'),
+             ('p','Viana do Castelo'),
+             ('q','Vila Real'),
+             ('r','Viseu')
+             ]
+
+NATUREZA=[
+    ('0', ''),
+    ('a', 'Protecção e Assistência a Pessoas e Bens / Assistência em Saúde / Trauma'),
+    ('b', 'Protecção e Assistência a Pessoas e Bens / Assistência em Saúde / Intoxicação'),
+    ('c', 'Riscos Tecnológicos / Acidentes / Despiste'),
+    ('d', 'Riscos Mistos / Incêndios em Detritos / Detritos confinados'),
+    ('e', 'Riscos Tecnológicos / Acidentes / Colisão rodoviária'),
+    ('f', 'Riscos Tecnológicos / Incêndios Urbanos ou em Área Urbanizável / Habitacional'),
+    ('g', 'Riscos Tecnológicos / Acidentes / Atropelamento rodoviário'),
+    ('h', 'Protecção e Assistência a Pessoas e Bens / Intervenção em conflitos legais / Agressão/Violação'),
+    ('i', 'Riscos Tecnológicos / Acidentes industriais e tecnológicos / Fuga de Gás em garrafa'),
+    ('j', 'Riscos Tecnológicos / Incêndios Urbanos ou em Área Urbanizável / Edifícios degradados ou devolutos'),
+    ('k', 'Protecção e Assistência a Pessoas e Bens / Assistência em Saúde / Intoxicação'),
+    ('l', 'Protecção e Assistência a Pessoas e Bens / Intervenção em conflitos legais / Suicídio/Homicídio na forma tentada'),
+    ('m', 'Riscos Mistos / Comprometimento total ou parcial de segurança, serviços ou estruturas / Inundação de estruturas por água canalizada'),
+    ('n', 'Protecção e Assistência a Pessoas e Bens / Assistência e Prevenção a actividades humanas / Limpeza de Via e Sinalização de Perigo'),
+    ('o', 'Riscos Mistos / Comprometimento total ou parcial de segurança, serviços ou estruturas / Queda de Árvore'),
+    ('p', 'Protecção e Assistência a Pessoas e Bens / Assistência e Prevenção a actividades humanas / Busca e Resgate Terrestre, de Animais'),
+    ('q', 'Protecção e Assistência a Pessoas e Bens / Assistência e Prevenção a actividades humanas / Limpeza de Via e Sinalização de Perigo'),
+]
+
+class RelatarForm(forms.Form):
+    data_ocorrencia = forms.DateField(label='Data da ocorrência', input_formats=['%d-%m-%Y'], widget=DatePickerInput(format='%d-%m-%Y', attrs={'class': 'form-control'}))
+    natureza = forms.CharField(label='Natureza', widget=forms.Select(choices=NATUREZA, attrs={'class': 'form-control'}))
+    estado = forms.CharField(label='Estado', widget=forms.Select(choices=STATE, attrs={'class': 'form-control'}))
+    distrito = forms.CharField(label='Distrito', widget=forms.Select(choices=DISTRITOS, attrs={'class': 'form-control'}))
+    concelho = forms.CharField(label='Concelho', widget=forms.TextInput(attrs={'class': 'form-control'}))
+    freguesia = forms.CharField(label='Freguesia', widget=forms.TextInput(attrs={'class': 'form-control'}))
+    localidade = forms.CharField(label='Localidade', widget=forms.TextInput(attrs={'class': 'form-control'}))
+    latitude = forms.FloatField(label='Latitude', widget=forms.NumberInput(attrs={'class': 'form-control'}))
+    longitude = forms.FloatField(label='Longitude', widget=forms.NumberInput(attrs={'class': 'form-control'}))
+    meiosTerrestres = forms.IntegerField(label='Meios Terrestres', widget=forms.NumberInput(attrs={'class': 'form-control'}))
+    opTerrestres = forms.IntegerField(label = 'Operacionais Terrestres', widget=forms.NumberInput(attrs={'class': 'form-control'}))
+    meiosAereos = forms.IntegerField(label = 'Meios Aéreos', widget=forms.NumberInput(attrs={'class': 'form-control'}))
+    opAereos = forms.IntegerField(label = 'Operacionais Aéreos', widget=forms.NumberInput(attrs={'class': 'form-control'}))
+
---
+++ webapp/app/views.py
@@ -0,0 +1,107 @@
+import lxml.etree as LET #to use on xslt
+from django.http import HttpResponse
+from django.shortcuts import render
+from .forms import RelatarForm
+from lxml import etree
+
+def index(request):
+    return render(request, 'index.html')
+
+def relatar(request):
+    form = RelatarForm()
+    return render(request, 'relatar.html', {'form': form})
+    #return render(request, 'relatar.html')
+
+
+def estatisticas(request):
+    return render(request, 'estatisticas.html')
+
+
+def avisos(request):
+    return render(request, 'avisos.html')
+
+
+def storeData(request):
+    if request.method == 'POST':
+        form = RelatarForm(request.POST)
+        print(form.errors)
+        if form.is_valid():
+            print(form)
+            data = form.cleaned_data
+            for k in data:
+                print('{} : {}'.format(k, data[k]))
+            return HttpResponse(status=204)
+            #return HttpResponseRedirect('/thanks/')
+        else:
+            print('not valid')
+            #return HttpResponseRedirect('/thanks/')
+    return HttpResponse(status=204)
+
+
+#validate a xml file with xml schema
+def validate(file_xml: str, file_schema: str):
+    # parsing xsd
+    with open(file_schema, 'r') as schema_file:
+        xmlschema_doc = etree.parse(schema_file)
+        xmlschema = etree.XMLSchema(xmlschema_doc)
+
+        # parsing xml
+        with open(file_xml, 'r') as xml_file:
+            try:
+                doc = etree.parse(xml_file)
+                print('XML well formed, syntax ok.')
+
+            # check for file IO error
+            except IOError:
+                print('Invalid File')
+
+            # check for XML syntax errors
+            except etree.XMLSyntaxError as err:
+                print('XML Syntax Error, see error_syntax.log')
+                with open('error_syntax.log', 'w') as error_log_file:
+                    error_log_file.write(str(err.error_log))
+                quit()
+
+            except:
+                print('Unknown error, exiting.')
+                quit()
+
+        # validate against schema
+        try:
+            xmlschema.assertValid(doc)
+            print('XML valid, schema validation ok.')
+
+        except etree.DocumentInvalid as err:
+            print('Schema validation error, see error_schema.log')
+            with open('error_schema.log', 'w') as error_log_file:
+                error_log_file.write(str(err.error_log))
+            quit()
+
+        except:
+            print('Unknown error, exiting.')
+            quit()
+
+        # validate schema
+        try:
+            xmlschema.assertValid(doc)
+            print('XML valid, schema validation ok.')
+
+        except etree.DocumentInvalid as err:
+            print('Schema validation error, see error_schema.log')
+            with open('error_schema.log', 'w') as error_log_file:
+                error_log_file.write(str(err.error_log))
+            quit()
+
+        except:
+            print('Unknown error, exiting.')
+            quit()
+
+
+#pagina html gerada com xslt depois dos dados serem inserios através do formulario (já passaram por uma verificação)
+# esta pagina apenas vai indicar se o incendio relatado foi aceite ou nao
+#def confirmData():
+
+
+
+
+
